c898bbb3caa07f45d6b6bb83082d212fa2fceaf8
pranavpawarr <pranavpawar503@gmail.com>
2025-10-28 16:19:59 +0530

    audio_player done // todo: pause and resume later

---
+++ src/audio_player.ts
@@ -0,0 +1,71 @@
+import player from "play-sound";
+
+export class AudioPlayer {
+  private currentTrack?: string;
+  private isPlaying: boolean = false;
+  private volume: number = 1.0;
+
+  private player: ReturnType<typeof player>;
+  private currentAudio: ReturnType<ReturnType<typeof player>["play"]> | null =
+    null;
+
+  constructor() {
+    this.player = player({});
+  }
+
+  async play(trackPath: string): Promise<void> {
+    if (this.isPlaying && this.currentAudio) {
+      this.currentAudio.kill();
+    }
+
+    this.currentTrack = trackPath;
+    this.isPlaying = true;
+
+    this.currentAudio = this.player.play(trackPath, (err) => {
+      if (err && !err.killed) {
+        console.error("Playback error:", err);
+      }
+      this.isPlaying = false;
+      this.currentAudio = null;
+    });
+  }
+
+  public stop(): void {
+    if (this.isPlaying && this.currentAudio) {
+      this.currentAudio.kill();
+    }
+    this.isPlaying = false;
+    this.currentTrack = undefined;
+    this.currentAudio = null;
+  }
+
+  public restart(): void {
+    if (this.currentTrack) {
+      this.play(this.currentTrack);
+    }
+  }
+
+  public setVolume(level: number): void {
+    if (level > 50) {
+      console.log(`too loud`);
+      this.volume = 50;
+    } else if (level < 0) {
+      console.log(`cant go lower than 0`);
+      this.volume = 0;
+    } else {
+      this.volume = level;
+    }
+  }
+
+  public getVolume(): number {
+    return this.volume;
+  }
+
+  public getCurrentTrack(): string | undefined {
+    return this.currentTrack;
+  }
+
+  public isCurrentlyPlaying(): boolean {
+    return this.isPlaying;
+  }
+}
